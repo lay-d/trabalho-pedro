8f90a607186e190653a16d3a6864385376b6ba34
lay-d <lays.diachuk.pereira@escola.pr.gov.br>
2025-11-24 11:38:25 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,18 +1,25 @@
 <?php
+// Configurações do banco de dados
+$host    = "localhost"; // servidor do banco
+$usuario = "root";      // usuário do banco
+$senha   = "";          // senha do bancoS
+$banco   = "lays_1d ";     // nome do banco de dados
 
-
-$host    = "localhost";   
-$usuario = "root";        
-$senha   = "";            
-$banco   = "lays_1d";       
-
-
+// Conexão MySQLi
 $conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
+// Verifica se a conexão funcionou
 if (!$conexao) {
     die("Erro ao conectar: " . mysqli_connect_error());
 }
 
-
+// Opcional: definir charset para evitar problemas com acentos
 mysqli_set_charset($conexao, "utf8");
+
+// Substituam os valores abaixo pelas credenciais da sua própria conta do Cloudinary
+$cloud_name = "dwiwcjyni";  // exemplo: "meucloud123"
+$api_key    = "356574788742319";     // exemplo: "123456789012345"
+$api_secret = "F_CYNootw1vWgDtX5X73CKr4xlQ";  // exemplo: "abcdeFGHijkLMNopqrstu"
+
 ?>
+
